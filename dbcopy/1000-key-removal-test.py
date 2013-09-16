cb2b2e1909577d3dd14179d0b6ff85a5e0d9dce1
Paul J. Davis <paul.joseph.davis@gmail.com>
2013-09-16 14:34:00 -0500

    Working dbcopy test for key removal

--- dbcopy/1000-key-removal-test.py
+++ dbcopy/1000-key-removal-test.py
@@ -26,50 +26,54 @@ def test_removal():
 
     db.doc_save(DDOC)
 
-    doc1 = db.doc_save({})
+    # Show that adding rows to a view udpates dbcopy correctly
+
+    doc1 = db.doc_save({}, w=3)
+
     v = db.view("bar", "bam", group=True)
     t.eq(len(v.rows), 1, "There should be a single row in the view")
     t.eq(v.rows[0]["value"], 1, "The row should have a value of 1")
-    
+
     db.wait_for_indexers(design_doc="bar")
 
-    v = copydb.all_docs(include_docs=True)
+    v = dbcopy.all_docs(include_docs=True)
     t.eq(len(v.rows), 1, "There should be a single doc in the dbcopy db")
     t.eq(v.rows[0]["doc"]["value"], 1, "The dbcopy value should be 1")
 
-    doc2 = db.doc_save({})
-    view = db.view("bar", "bam", group=True)
+    doc2 = db.doc_save({}, w=3)
+
+    v = db.view("bar", "bam", group=True)
     t.eq(len(v.rows), 1, "There should be a single row in the view")
     t.eq(v.rows[0]["value"], 2, "The single row should have a new value")
 
     db.wait_for_indexers(design_doc="bar")
 
-    view = copydb.all_docs(include_docs=True)
+    v = dbcopy.all_docs(include_docs=True)
     t.eq(len(v.rows), 1, "There should still only be a single dbcopy doc")
-    t.eq(v.rows["doc"]["value"], 2, "The dbcopy doc should have a new value")
+    t.eq(v.rows[0]["doc"]["value"], 2, "The dbcopy doc should have a new value")
 
     # Make sure that deleting one of the source documents
     # updates the view and dbcopy doc appropriately
-    db.delete(doc1)
+    db.doc_delete(doc1, w=3)
 
     v = db.view("bar", "bam", group=True)
     t.eq(len(v.rows), 1, "There should be a single view row")
     t.eq(v.rows[0]["value"], 1, "The value should be 1 again")
 
     db.wait_for_indexers(design_doc="bar")
-    
-    v = copydb.all_docs(include_docs=True)
+
+    v = dbcopy.all_docs(include_docs=True)
     t.eq(len(v.rows), 1, "There should be a single doc in the dbcopy db")
     t.eq(v.rows[0]["doc"]["value"], 1, "The dbcopy doc value should be 1")
 
     # Make sure removing the last doc from the source view
     # results in an empty view and dbcopy db
-    db.delete(doc2)
+    db.doc_delete(doc2, w=3)
 
     v = db.view("bar", "bam", group=True)
     t.eq(len(v.rows), 0, "There should be no more rows in the view")
 
     db.wait_for_indexers(design_doc="bar")
 
-    v = copydb.all_docs()
+    v = dbcopy.all_docs()
     t.eq(len(v.rows), 0, "There should be no more docs in the dbcopy db")

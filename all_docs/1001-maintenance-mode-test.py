59f30aefec73ab361452aba9742084ee07e8556a
Paul J. Davis <paul.joseph.davis@gmail.com>
2013-10-04 17:29:00 -0500

    Tests for _all_docs during maintenance mode

---
+++ all_docs/1001-maintenance-mode-test.py
@@ -0,0 +1,45 @@
+
+from hamcrest import *
+
+import cloudant
+
+
+NUM_ROWS = 100
+
+
+def setup_module():
+    srv = cloudant.get_server()
+    db = srv.db("test_suite_db")
+    db.reset(q=4)
+    docs = []
+    for i in range(NUM_ROWS):
+        docs.append({"value":i})
+    db.bulk_docs(docs)
+
+
+def test_all_docs():
+    # Check that we can run with maintenance mode on a number
+    # of servers. This has an assumption that the last node in
+    # the cluster has a fully shard ring. To make this more better
+    # we should compare the list of nodes returned to the shard
+    # map so that we can remove non-shard-containing nodes from
+    # service first.
+    srv = cloudant.get_server()
+    db = srv.db("test_suite_db")
+    nodes = cloudant.nodes(interface="public")
+    try:
+        for n in nodes[:-1]:
+            n.config_set("cloudant", "maintenance_mode", "true")
+            v = db.all_docs()
+            assert_that(v.rows, has_length(100))
+        n = nodes[-1]
+        n.config_set("cloudant", "maintenance_mode", "true")
+        try:
+            db.all_docs()
+        except:
+            assert_that(srv.res.last_req.json(), has_entry("error", "nodedown"))
+        else:
+            raise AssertionError("View should not complete successfully")
+    finally:
+        for n in nodes:
+            n.config_set("cloudant", "maintenance_mode", "false")
